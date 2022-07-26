9029fac2aa703f3bed2b51e8f54edd886ca999c9
Omar Aizab <omaraizab98@gmail.com>
2022-07-26 16:22:15 +0100

    fix c07 ex01 for const

--- c++07/ex01/iter.hpp
+++ c++07/ex01/iter.hpp
@@ -4,6 +4,13 @@
 #include <iostream>
 
 template <typename T>
+void iter(T const *t, int size, void (*f)(T const &))
+{
+	for (int i = 0; i < size; i++)
+		f(t[i]);
+}
+
+template <typename T>
 void iter(T *t, int size, void (*f)(T &))
 {
 	for (int i = 0; i < size; i++)
